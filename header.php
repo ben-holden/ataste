3cd80f1be3cf418d5834378eea7fe2d046f84d3d
NotQuiteHere <tamarisk@justvigilantes.co.uk>
2014-11-19 10:47:49 +0000

    made the header clickable again

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<div class="headertitlescontainer">
-				<div class="headertitles"></div>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><div class="headertitles"></div></a>
 			</div>		
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="season"><?php bloginfo( 'description' ); ?></h2>
